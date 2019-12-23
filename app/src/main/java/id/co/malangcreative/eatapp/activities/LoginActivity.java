f694c6554b5b4111b2308d7d9379570ca3106670
Arka Arifiandi Leonanta <arkaarif69@gmail.com>
2019-12-23 08:12:57 +0700

    make login_activity

---
+++ app/src/main/java/id/co/malangcreative/eatapp/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package id.co.malangcreative.eatapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.co.malangcreative.eatapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
